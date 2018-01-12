158aaf3c4dc1f54084560f416d0c330c6eb69b43
Josh Lavin <digory@cpan.org>
2018-01-12 08:34:54 -0800

    Rename, and skip if cert is < 2 days old

--- 2get_ssl.sh
+++ 2get_ssl.sh
@@ -1,4 +1,7 @@
 for s in `cat sites`
 do
+    `find ssl_output -name $s -mtime +2` && continue
+    echo ""
+    echo "Doing $s..."
     openssl s_client -connect $s:443 -servername $s </dev/null > ssl_output/$s
 done
